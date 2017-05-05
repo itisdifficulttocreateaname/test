e2dd609bd223a8f2aee06fceb2ddb29d666267fd
Zitong Zhang <3140104378@zju.edu.cn>
2017-05-05 11:42:23 +0800

    reconstructed

--- SWC/TreeNodes.py
+++
@@ -1,7 +0,0 @@
-
-def AllTreeNodes(tree):
-	return (tree, ) + tree.descendants
-
-
-def TreeLeaves(tree):
-    return (leaf for child in tree.children for leaf in TreeLeaves(child)) if not tree.is_leaf else (tree,)
