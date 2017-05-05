ed99cf50767b880f9b6370057398f91dbd1baa5e
Zitong Zhang <3140104378@zju.edu.cn>
2017-05-05 20:36:06 +0800

    reconstructed

--- SWC的副本/code/TreeParaRescale.py
+++
@@ -1,7 +0,0 @@
-from TreeNodes import AllTreeNodes
-
-def TreeParaRescale(tree, **dict):
-	all_nodes = AllTreeNodes(tree)
-	for para in ('x', 'y', 'z', 'r'):
-		if para in dict:
-			map(lambda node: node.para_rescale(para, dict[para]), all_nodes)
